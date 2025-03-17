31096e45a38fc52029815c83032d920c6e81464d
JeyanthiniSubendiran <jeyanthinisubendiran@gmail.com>
2025-03-17 11:11:19 +0530

    Fix: changed file name

--- something.java
+++ something.java
@@ -1,4 +1,4 @@
-public class java{
+public class something{
     public static void main(Strings args[]){
         system.out.println("Hey there! Its Jeyanthini")
         system.out.println("I'm new to code")
